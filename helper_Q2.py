6ca195b7fea5239e9988a90c6dcf6cee4d335f07
Megha <32072659+meghamishra@users.noreply.github.com>
2020-05-02 13:14:40 -0400

    Add files via upload
    Added PCA component

--- helper_Q2.py
+++ helper_Q2.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 25 13:33:35 2020
-
-@author: megha
-"""
-
+## Final
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+# from sklearn.preprocessing import StandardScaler
 
 
-def cleaning_data(df):
+def cleaning_data(df,reduced):
     
     df_dum=pd.get_dummies(df['titleType'])
     df=pd.merge(df,df_dum,left_index=True,right_index=True)
@@ -18,13 +14,18 @@ def cleaning_data(df):
     df_curated=df.drop(['tconst','titleType','originalTitle'],axis=1)    
     df_curated['startYear']=df_curated['startYear']/df_curated['startYear'].max()    
     df_curated['runtimeMinutes']=(df_curated['runtimeMinutes']-df_curated['runtimeMinutes'].min())/(df_curated['runtimeMinutes'].max()-df_curated['runtimeMinutes'].min())
+    cleaned_arr=np.array(df_curated)
+    if reduced==1:
+        
+        cleaned_arr=cleaned_arr-np.mean(cleaned_arr,axis=0)
+        pca=PCA(n_components=20)
+        cleaned_arr=pca.fit_transform(cleaned_arr)
     
-    return df_curated
-
+    return cleaned_arr
 def k_means_plus_centriods_initialization(num_clus,arr):
     centers = []
     i1 = np.random.randint(0,arr.shape[0])
-    # num_clus = 4
+
     centers.append(arr[i1])
     for c in range(num_clus-1):
         sum_dists = 0
@@ -55,7 +56,7 @@ def k_means_centriods(num_clus,arr,iters,batch_size,lr,centroids):
         
         for i in range(num_clus):   
             if (closest_center_arg==i).any(): 
-                centroids[i] = centroids[i] - lr*(np.mean(mini_batch[closest_center_arg==i],axis = 0)-centroids[i])
+                centroids[i] = centroids[i] + lr*(np.mean(mini_batch[closest_center_arg==i],axis = 0)-centroids[i])
                 
                 
     return centroids
@@ -63,81 +64,82 @@ def k_means_centriods(num_clus,arr,iters,batch_size,lr,centroids):
 
 def make_clusters(arr,centroids,num_clus):
         clusters=[[] for i in range(num_clus)]
+        labels=[]
         for num in arr:
             dist=np.sum((num-centroids)**2,axis=1)
-            cluster_value=np.argmin(dist)
-            clusters[cluster_value].append(num)
             
-        return clusters
-    
-def min_max_mean_plot(clusters,centroids,num_clus):
-    plot_mean=[]
-    plot_min=[]
-    plot_max=[]   
-    for c in range(num_clus): 
-        sum_mean=0
-        if len(clusters[c])>0:
-            plot_mean.append(sum((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5)/len(clusters[c]))
-            plot_max.append(max((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5))
-            plot_min.append(min((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5))
+            cluster_value=np.argmin(dist)
            
-    print('plots for Cluster value '+str(num_clus) )    
-    print("mean value plot")
-    plt.plot(plot_mean)
-    plt.show()
-    print("max value plot")
-    plt.plot(plot_max)
-    plt.show()
-    print("min value plot")
-    plt.plot(plot_min)
-    plt.show()
+            clusters[cluster_value].append(num)
+            labels.append(cluster_value)
+        return clusters,labels
     
+def cluster_plot(arr,labels_arr,reduced):
+    if reduced==2:
+        ssfit=arr-np.mean(arr,axis=0)
+        pca=PCA(n_components=2)
+        arr=pca.fit_transform(ssfit)
+
+    plt.scatter(arr[:,0],arr[:,1],c=labels_arr)
+    plt.xlabel('PCA_Component1')
+    plt.ylabel('PCA_Component2')
+    plt.show() 
     
+
+   
 def plot_curves_k_means(clus_values,arr,iters,batch_size,lr,selection): 
     elbow_value=[]
+    min_value=[]
+    max_value=[]
+    mean_value=[]
     
     for num_clus in clus_values: 
-          
         plot_mean=[]
-        plot_min=[]
-        plot_max=[]
-        
+             
         if selection==1:
             centers_ind = np.random.randint(0,len(arr),num_clus)    
             centroids=arr[centers_ind] 
+            str_title="K-means"
         else:
             centroids= k_means_plus_centriods_initialization(num_clus,arr)
+            str_title="K-means++"
             
         centroids=k_means_centriods(num_clus,arr,iters,batch_size,lr,centroids)
-        clusters=make_clusters(arr,centroids,num_clus)
+        clusters,labels=make_clusters(arr,centroids,num_clus)        
         sum_val=0
+        
         for c in range(num_clus): 
-            sum_mean=0
             if len(clusters[c])>0:
-                sum_val=sum_val+sum((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5)
-                plot_mean.append(sum((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5)/len(clusters[c]))
-                plot_max.append(max((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5))
-                plot_min.append(min((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5))
-               
-        print('plots for Cluster value '+str(num_clus) )    
-        print("mean value plot")
-        plt.plot(plot_mean)
-        plt.show()
-        print("max value plot")
-        plt.plot(plot_max)
-        plt.show()
-        print("min value plot")
-        plt.plot(plot_min)
-        plt.show()
     
-        elbow_value.append(sum_val)    
-    
+                dist_value=sum((np.sum((clusters[c]-centroids[c])**2,axis=1))**0.5)
+                sum_val=sum_val+dist_value
+                plot_mean.append(dist_value/len(clusters[c]))
+            else:
+                plot_mean.append(0)
+
+
+        elbow_value.append(sum_val)   
+        min_value.append(min(plot_mean))  
+        max_value.append(max(plot_mean))
+        mean_value.append(np.mean(plot_mean))
+
     print("elbow curve")
     plt.plot(clus_values,elbow_value)
+    plt.xlabel('number of clusters')
+    plt.ylabel('intra cluster distance')
+    plt.title(str_title)
+    plt.show()
+
+    plt.plot(clus_values,mean_value,label='mean')
+    plt.plot(clus_values,max_value,label='max')
+    plt.plot(clus_values,min_value,label='min') 
+    plt.legend()
+    plt.xlabel('number of clusters')
+    plt.ylabel('distance values')
+    plt.title(str_title)   
+    
     plt.show()
         
-        
-
 
             
 
--- mainQ2_final.py
+++ mainQ2_final.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 25 13:29:28 2020
-
-@author: megha
-"""
+## Final
 
 import numpy as np
 import pandas as pd
@@ -15,43 +10,42 @@ pd.set_option('display.max_columns', 500)
 
 ########## DEFINING HYPER PARAMETERS ##########################################
 
-batch_size = 100
-iters = 100
+batch_size = 1000
+iters = 1000
 lr = 0.01
-selection=2 # 1 for random initialization and 2 for k-means++ initialization
+reduced=2 # 1 for PCA reduced data and 2 for original data
 
 ############################# Reading dataset #################################
 df=pd.read_csv(r"Movies.csv")
 
 #######################Getting data in required format ########################
-cleaned_arr=np.array(cleaning_data(df))
+cleaned_arr=cleaning_data(df,reduced)
 
-
-###################### Implementing  K-means algorithm #########################
+###### Implementing  K-means algorithm with Random Initialization##############
 num_clus=10
 centers_ind = np.random.randint(0,len(cleaned_arr),num_clus)    
 centroids=cleaned_arr[centers_ind] 
 centroids=k_means_centriods(num_clus,cleaned_arr,iters,batch_size,lr,centroids)
-clusters=make_clusters(cleaned_arr,centroids,num_clus)
+clusters,labels=make_clusters(cleaned_arr,centroids,num_clus)
 
 ### Plotting clusters###
+print("clusters obtained for K-means with Random Initialization")
+cluster_plot(cleaned_arr,labels,reduced)
 
-min_max_mean_plot(clusters,centroids,num_clus)
 
-
-###################### Implementing  K-means++ algorithm #########################
+###### Implementing  K-means algorithm with Kmeans++ Initialization ###########
 num_clus=10  
 centroids= k_means_plus_centriods_initialization(num_clus,cleaned_arr)
 centroids=k_means_centriods(num_clus,cleaned_arr,iters,batch_size,lr,centroids)
-clusters=make_clusters(cleaned_arr,centroids,num_clus)
+clusters,labels=make_clusters(cleaned_arr,centroids,num_clus)
 
 ### Plotting clusters###
+print("clusters obtained for K-means with Kmeans++ Initialization")
+cluster_plot(cleaned_arr,labels,reduced)
 
-min_max_mean_plot(clusters,centroids,num_clus)
-
-###################### K-MEANS WITH curves #####################
-clus_values=[5,20,100,500]
-
+###################### K-MEANS WITH CURVES ####################################
+clus_values=[5,20,50,100,300,400,500]
+selection=1 # 1 for random initialization and 2 for k-means++ initialization
 plot_curves_k_means(clus_values,cleaned_arr,iters,batch_size,lr,selection)
 
         
